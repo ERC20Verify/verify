87c6ced85cbd7ad0988f06950ee7eeecb921718d
ERC20Verify <106013074+WalletSecure-git@users.noreply.github.com>
2022-08-09 09:49:46 +0100

    Add files via upload

---
+++ etf.py
@@ -0,0 +1,9 @@
+import math
+def indivclass (str):
+    indivclass= '100 level', '200 level', '300 level', '400 level'
+    studentsname= 'mike', 'john', 'bill'
+    (indivclass + studentsname)
+studentsname='mike', 'john', 'bill'
+name=indivclass(studentsname)
+print(name)
+
